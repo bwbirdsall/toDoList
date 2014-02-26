89740f67b3e4f9c3b752fabac204eb83f4d445cb
Spyder Monkee McHenry & Ben Birdsall <student@epicodus.com>
2014-02-25 17:09:44 -0800

    add ability to manipulate lists.

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,9 @@
 var List = {
 };
-
-var Task = {
-
-};
+var listOfLists = [];
 
 $(document).ready(function() {
-  $("#add-task").click(function() {
-    $("#list-tasks").append('<div class="new-list-task">' +
-                              '<div class="form-group">' +
-                                '<label for="new-task">To Do Task</label>' +
-                                '<input type="text" class="form-control new-task">' +
-                              '</div>' +
-                            '</div>');
-    });
-
+  var currentList = Object.create(List);
   $("form#new-list").submit(function(event) {
     event.preventDefault();
 
@@ -24,27 +13,33 @@ $(document).ready(function() {
     newList.name = inputtedList;
     newList.tasks = [];
 
-    $(".new-list-task").each(function() {
-      newList.tasks.push($(this).find("input.new-task").val());
-    });
-
-    console.log(newList.tasks);
-
+    listOfLists.push(newList);
     $("ul#lists").append("<li><span class='list'>" + newList.name + "</span></li>");
 
     $("#show-lists").show();
 
     $('.list').last().click(function(){
+      console.log(newList.name);
+      currentList = newList;
       $("#show-tasks").show();
-      
       $("#show-tasks h2").text(newList.name);
+      $("ul#tasks").text("");
       newList.tasks.forEach(function(task){
         $("ul#tasks").append("<li><span class='task'>" + task + "</span></li>");
       });  
     });
-    
 
 
     this.reset();
+
   });
+
+  $("form#new-tasks").submit(function(event) {
+    event.preventDefault();
+    var task = $("input#new-task").val();
+    currentList.tasks.push(task);
+    $("ul#tasks").append("<li><span class='task'>" + task + "</span></li>");
+    this.reset();
+  });
+    
 });
